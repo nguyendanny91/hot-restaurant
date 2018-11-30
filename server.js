c243af55bed37d49aa1bebac7a944c80a2ee3a00
Vu Duong <vuduong191@gmail.com>
2018-11-29 20:43:36 -0800

    adjust data

--- server.js
+++ server.js
@@ -14,27 +14,44 @@ app.use(express.json());
 
 // Star Wars Characters (DATA)
 // =============================================================
-var characters = [
+var reservedCust = [
   {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
+    customerName: "Marcellus-reserved",
+    phoneNumber: "1-800-SrDev55",
+    customerEmail: "Seniordev@eng.com",
+    customerID: "4124"
   },
   {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
+    customerName: "Marcellus-reserved",
+    phoneNumber: "1-800-SrDev55",
+    customerEmail: "Seniordev@eng.com",
+    customerID: "4124"
   },
   {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
+    customerName: "Marcellus-reserveds",
+    phoneNumber: "1-800-SrDev55",
+    customerEmail: "Seniordev@eng.com",
+    customerID: "4124"
+  }
+];
+var waitlistCust = [
+  {
+    customerName: "Marcellus-waitlist",
+    phoneNumber: "1-800-SrDev55",
+    customerEmail: "Seniordev@eng.com",
+    customerID: "4124"
+  },
+  {
+    customerName: "Marcellus-waitlist",
+    phoneNumber: "1-800-SrDev55",
+    customerEmail: "Seniordev@eng.com",
+    customerID: "4124"
+  },
+  {
+    customerName: "Marcellus-waitlist",
+    phoneNumber: "1-800-SrDev55",
+    customerEmail: "Seniordev@eng.com",
+    customerID: "4124"
   }
 ];
 
@@ -47,11 +64,11 @@ app.get("/", function(req, res) {
 });
 
 app.get("/tables", function(req, res) {
-  res.sendFile(path.join(__dirname, "table.html"));
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
+    res.sendFile(path.join(__dirname, "waitlist.html"));
   });
 
 
@@ -60,54 +77,19 @@ app.get("/reserve", function(req, res) {
 
 // Displays all characters
 app.get("/table#", function(req, res) {
-  return res.json(characters);
+  return res.json(reservedCust);
   
 });
 
-app.get("/api.table", function(req, res) {
-    return res.json(characters);
+app.get("/api/table", function(req, res) {
+    return res.json(reservedCust);
     
   });
 
-app.get("/waitlist", function(req, res) {
-    return res.json(characters);
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlistCust);
     
   }); 
-
-
-
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
-
-  console.log(chosen);
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
-});
-
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newcharacter = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newcharacter);
-
-  characters.push(newcharacter);
-
-  res.json(newcharacter);
-});
-
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
